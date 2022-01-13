e8392c00f956d7555cbd958b7a7903f8e717a6bb
SapirAbutbulMoveo <sapira@moveo.co.il>
2022-01-13 14:26:48 +0200

    final version

--- football/src/components/Teams.tsx
+++ football/src/components/Teams.tsx
@@ -5,39 +5,62 @@ import EachTeam from './EachTeam.tsx';
 
 
 function Teams (props) {
-  class Team {
-    constructor(
-     readonly teamName:string,
-     readonly teamLogo: string,
-    ){}
+  interface Team { 
+     teamName:string;
+     teamLogo: string;
+    }
     
-  }
       return (
-
       <div>
-
         <div className='teams-container'>
           {props.team.map((element, i) => {
-            const eachTeam = new Team(element.strTeam, element.strTeamLogo)
+            const eachTeam : Team={
+             teamName: element.strTeam,
+             teamLogo: element.strTeamLogo
+              }
             return <EachTeam key={i} team = {eachTeam}/>
           })}
         </div>
-
-        
-
       </div>
     );
   
-
-    // return (
-    //   <div>
-    //       <h1>hello</h1>
-    //       <EachTeam/>
-    //   </div>
-    
-    // )
-    
   };
   
   export default Teams;
+  
+
+
+//   import React from 'react';
+// import EachTeam from './EachTeam.tsx';
+
+
+
+
+// function Teams (props) {
+//   class Team {
+//     constructor(
+//      readonly teamName:string,
+//      readonly teamLogo: string,
+//     ){}
+    
+//   }
+//       return (
+
+//       <div>
+
+//         <div className='teams-container'>
+//           {props.team.map((element, i) => {
+//             const eachTeam = new Team(element.strTeam, element.strTeamLogo)
+//             return <EachTeam key={i} team = {eachTeam}/>
+//           })}
+//         </div>
+
+        
+
+//       </div>
+//     );
+  
+//   };
+  
+//   export default Teams;
   
